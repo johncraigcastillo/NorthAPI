d25824acbdd2bc6064785eaa986727648a773159
johncastillo <johncraigcastillo@gmail.com>
2023-10-21 21:30:50 -0500

    Initial commit

---
+++ NorthAPI/Program.cs
@@ -0,0 +1,29 @@
+using MySql.Data.MySqlClient;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register MySqlConnection through PlanetScale
+builder.Services.AddTransient<MySqlConnection>(_ =>
+    new MySqlConnection(builder.Configuration["ConnectionStrings:Default"]));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
